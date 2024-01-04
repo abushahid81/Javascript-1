2950cbd4bfcb2ade2a2bc4511ab486ae6d985e95
abushahid81 <abushahidsardar81@gmail.com>
2024-01-04 11:28:39 -0800

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+// let js = "My name";
+// if (js === "My name")
+// alert("Abushahid Sardar");
+
+// console.log(50 + 90 - 20 * 6);
+
+// let n = '2' + '1';
+// console.log(n);
+
+let time = 0;
+if (time){
+    console.log("Don't spend it all;)");
+}else{
+    console.log('You should get a job!');
+}
+// second
+
+let money = 10;
+if (money){
+    console.log("Don't spend it all");
+}else{
+    console.log('You should get a job!');
+}
